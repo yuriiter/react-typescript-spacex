ce4065a21b0451b4e6d9f535d6e63022e6d2e37e
Yurii Tereshchenko <yurater007@gmail.com>
2022-11-21 11:50:41 +0100

    triv: fix for vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: [/^node:.*/],
+      external: ['@emotion/react'],
     },
   },
 });
